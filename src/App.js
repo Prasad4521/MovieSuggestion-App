ca48d7d914d1351540cd2ba41e87f837376f5622
Prasad <dp02377@gmail.com>
2024-02-26 18:15:07 +0530

    added movieslist

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-import {SafeAreaView, StatusBar, StyleSheet, Text, View,} from 'react-native';
+import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
 import MoviesList from './components/MoviesList';
 
 function App() {
   return (
-      <View>
-        <Logo/>
-        <Text> Tik Tak Toe With Nik </Text>
-      </View>
+    <View>
+      <Logo />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  
-});
+const styles = StyleSheet.create({});
 
 export default App;
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,51 +1,86 @@
-import React from 'react'
-import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MainScreen from './apiList';
 
 export default Logo = () => {
-    return (
-        <SafeAreaView>
-            <View style={styles.headingContainer}>
-                    <Text style={styles.headingText1}>Weekend Movie</Text>
-                <View style={styles.headerIcons}>
-                    <FontAwesome name="user" color="#ffffff" size={30}/>
-                    <FontAwesome name="love" color="#ffffff" size={30}/>
-                </View>
-            </View>
-            <View style={styles.languageButton}>
-                <TouchableOpacity>
-                    <Text style={styles.languageText}>English</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text style={styles.languageText}>Hind</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text style={styles.languageText}>Telugu</Text>
-                </TouchableOpacity>
-            </View>
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView>
+      <View style={styles.headingContainer}>
+        <Text style={styles.headingText}>Movie List</Text>
+        <View style={styles.headerLogo}>
+          <FontAwesome
+            name="user"
+            size={38}
+            color="#ffffff"
+            paddingRight={20}
+          />
+          <FontAwesome
+            name="heart"
+            size={38}
+            color="#ffffff"
+            paddingRight={0}
+          />
+        </View>
+      </View>
+      <View style={styles.languageBtn}>
+        <TouchableOpacity>
+          <Text style={styles.languagetxt}>English</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.languagetxt}>Hindi</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.languagetxt}>Tamil</Text>
+        </TouchableOpacity>
+      </View>
+      <MainScreen />
+    </SafeAreaView>
+  );
+};
 
-const styles= StyleSheet.create({
-    headingContainer:{
-        flexDirection:'row',
-        alignItems:'center',
-        width:'auto',
-        height:80,
-        backgroundColor:'#000000'
-    },
-    headerIcons:{
-        flex:1,
-        flexDirection:'row',
-        justifyContent:'space-evenly',
-        paddingRight:10
-    },
-    headingText1:{
-        fontSize:25,
-        fontWeight:'bold',
-        marginRight:120,
-        marginLeft:10,
-        color:'white'
-    },
-})
+const styles = StyleSheet.create({
+  headingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    padding: 10,
+  },
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginRight: 120,
+  },
+  headerLogo: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  languageBtn: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    fontSize: 24,
+    fontWeight: 'bold',
+    justifyContent: 'space-evenly',
+    height: 60,
+    backgroundColor: 'lightgrey',
+  },
+  languagetxt: {
+    margin: 2,
+    backgroundColor: 'blue',
+    color: 'white',
+    padding: 10,
+    height: 40,
+    width: 100,
+    borderRadius: 5,
+    paddingLeft: 30,
+  },
+});
